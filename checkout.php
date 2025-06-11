82e61e5bdff1ae77a5ec224f801af8f4d87e8630
Subash chand <subashchand@Subashs-MacBook-Air.local>
2025-06-11 16:19:01 +0545

    update checkout sucessfully

--- checkout.php
+++ checkout.php
@@ -202,7 +202,7 @@ session_start();
                                             }
                                         ?>
                                         <tr>
-                                            <td colspan="4" class="text-end py-5"><strong>Subtotal</strong></td>
+                                            <td colspan="4" class="text-end py-5"><strong><br>Subtotal </strong></td>
                                             <td class="py-5">
                                                 <div class="py-3 border-bottom border-top">
                                                     <strong>Rs <?php echo number_format($grandTotal, 2); ?></strong>
@@ -228,6 +228,14 @@ session_start();
                                 </div>
                             </div>
 
+                            <div class="row g-4 text-center align-items-center justify-content-center border-bottom py-3">
+                                <div class="col-12">
+                                    <div class="form-check text-start my-3">
+                                        <input type="checkbox" class="form-check-input bg-primary border-0" id="Transfer-1" name="Transfer" value="Transfer">
+                                        <label class="form-check-label" for="Transfer-1">Direct Bank Transfer</label>
+                                    </div>
+                                </div>
+
                             <!-- Place Order Button -->
                             <div class="row g-4 text-center align-items-center justify-content-center pt-4">
                                 <button type="submit" class="btn border-secondary py-3 px-4 text-uppercase w-100 text-primary">Place Order</button>
@@ -242,29 +250,29 @@ session_start();
 
 
 
-        <!-- Footer Start -->
+       <!-- Footer Start -->
         <div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5">
             <div class="container py-5">
-                <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(226, 175, 24, 0.5) ;">
+                <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(226, 175, 24, 0.5);">
                     <div class="row g-4">
                         <div class="col-lg-3">
-                            <a href="#">
-                                <h1 class="text-primary mb-0">Branded Clothes</h1>
-                                <p class="text-secondary mb-0">Fresh brand new products</p>
+                            <a href="index.php">
+                                <h1 class="text-primary mb-0">E-Clothing Store</h1>
+                                <p class="text-secondary mb-0">Trendy and Quality Clothing</p>
                             </a>
                         </div>
                         <div class="col-lg-6">
                             <div class="position-relative mx-auto">
-                                <input class="form-control border-0 w-100 py-3 px-4 rounded-pill" type="number" placeholder="Your Email">
+                                <input class="form-control border-0 w-100 py-3 px-4 rounded-pill" type="email" placeholder="Your Email">
                                 <button type="submit" class="btn btn-primary border-0 border-secondary py-3 px-4 position-absolute rounded-pill text-white" style="top: 0; right: 0;">Subscribe Now</button>
                             </div>
                         </div>
                         <div class="col-lg-3">
                             <div class="d-flex justify-content-end pt-3">
-                                <a class="btn  btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i class="fab fa-youtube"></i></a>
-                                <a class="btn btn-outline-secondary btn-md-square rounded-circle" href=""><i class="fab fa-linkedin-in"></i></a>
+                                
+                                <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href="https://www.facebook.com/subash.chand.202853"><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href="mailto: subashchan31@gmail.com"><i class="fas fa-envelope"></i></a>
+
                             </div>
                         </div>
                     </div>
@@ -272,42 +280,40 @@ session_start();
                 <div class="row g-5">
                     <div class="col-lg-3 col-md-6">
                         <div class="footer-item">
-                            <h4 class="text-light mb-3">Why People Like us!</h4>
-                            <p class="mb-4">typesetting, remaining essentially unchanged. It was 
-                                popularised in the 1960s with the like Aldus PageMaker including of Lorem Ipsum.</p>
-                            <a href="" class="btn border-secondary py-2 px-4 rounded-pill text-primary">Read More</a>
+                            <h4 class="text-light mb-3">Why Shop With Us?</h4>
+                            <p class="mb-4">We offer the best quality, latest fashion trends, and excellent customer service to keep you satisfied and stylish.</p>
+                            <a href="about.php" class="btn border-secondary py-2 px-4 rounded-pill text-primary">Read More</a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="d-flex flex-column text-start footer-item">
                             <h4 class="text-light mb-3">Shop Info</h4>
-                            <a class="btn-link" href="">About Us</a>
-                            <a class="btn-link" href="">Contact Us</a>
-                            <a class="btn-link" href="">Privacy Policy</a>
-                            <a class="btn-link" href="">Terms & Condition</a>
-                            <a class="btn-link" href="">Return Policy</a>
-                            <a class="btn-link" href="">FAQs & Help</a>
+                            <a class="btn-link" href="#">About Us</a>
+                            <a class="btn-link" href="#">Contact Us</a>
+                            <a class="btn-link" href="#">Privacy Policy</a>
+                            <a class="btn-link" href="#">Terms & Conditions</a>
+                            <a class="btn-link" href="#">Return Policy</a>
+                            <a class="btn-link" href="h#">FAQs & Help</a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="d-flex flex-column text-start footer-item">
                             <h4 class="text-light mb-3">Account</h4>
-                            <a class="btn-link" href="">My Account</a>
-                            <a class="btn-link" href="">Shop details</a>
-                            <a class="btn-link" href="">Shopping Cart</a>
-                            <a class="btn-link" href="">Wishlist</a>
-                            <a class="btn-link" href="">Order History</a>
-                            <a class="btn-link" href="">International Orders</a>
+                            <a class="btn-link" href="#">My Account</a>
+                            <a class="btn-link" href="s#">Shop Details</a>
+                            <a class="btn-link" href="#">Shopping Cart</a>
+                            <a class="btn-link" href="#">Wishlist</a>
+                            <a class="btn-link" href="orders.php">Order History</a>
+                            
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="footer-item">
                             <h4 class="text-light mb-3">Contact</h4>
-                            <p>Address: 1429 Netus Rd, NY 48247</p>
-                            <p>Email: Example@gmail.com</p>
-                            <p>Phone: +0123 4567 8910</p>
-                            <p>Payment Accepted</p>
-                            <img src="img/payment.png" class="img-fluid" alt="">
+                            <p>Address: Dhagadi, Kailali, Sudurpashchim, Nepal</p>
+                            <p>Email: support@eclothingstore.com</p>
+                            <p>Phone: +977 9844351869</p>
+
                         </div>
                     </div>
                 </div>
@@ -315,23 +321,24 @@ session_start();
         </div>
         <!-- Footer End -->
 
+
         <!-- Copyright Start -->
         <div class="container-fluid copyright bg-dark py-4">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        <span class="text-light"><a href="#"><i class="fas fa-copyright text-light me-2"></i>Your Site Name</a>, All right reserved.</span>
+                        <span class="text-light">
+                            <a href="#"><i class="fas fa-copyright text-light me-2"></i>E-Clothing Store</a>, All rights reserved.
+                        </span>
                     </div>
                     <div class="col-md-6 my-auto text-center text-md-end text-white">
-                        <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
-                        <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
-                        <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
-                        Designed By <a class="border-bottom" href="https://htmlcodex.com">DLMS Group</a>
+                        Designed By <a class="border-bottom" href="#">LMSD Group</a> 
                     </div>
                 </div>
             </div>
         </div>
         <!-- Copyright End -->
+
 
 
 
